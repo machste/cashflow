9954701808bd0499fc6897ad89971d69553b2da7
Stefan Mächler <stefan.maechler@westerm.com>
2024-08-01 14:00:39 +0200

    Add support for viewing categories and currencies

--- cashflow/views/categories.py
+++ cashflow/views/categories.py
@@ -1,10 +1,12 @@
 from pyramid.view import view_config
 
 from cashflow.views import View
+from cashflow.models.categories import Category
 
 
 class Categories(View):
 
     @view_config(route_name='categories', renderer='categories.jinja2')
     def view(self):
-        return {}
+        categories = self.dbsession.query(Category).all()
+        return dict(categories=categories)
